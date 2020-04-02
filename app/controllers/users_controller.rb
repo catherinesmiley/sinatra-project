13ab1f831299379d5d6a96056f68cb73e467e6f4
cesmiley <catherinesmiley@gmail.com>
2020-04-02 19:01:11 -0400

    fixed errors on signup form, added code to post signup route, created users/:id get route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
     end 
 
     post '/signup' do 
-        binding.pry 
+        user = User.create(params)
+        session[:user_id] = user.id
+        redirect to "/users/#{user.id}" 
+    end 
+
+    get '/users/:id' do 
+        if User.find_by(id: params [:id])
+            @user = User.find_by(id: params[:id])
+        else 
+            redirect to '/'
+        end 
+        erb :'/users/show'
     end 
 
 end 
